98882d2cc62989951de12c5d589935bb94435dfb
Sethiepooh <82126052+Sethiepooh@users.noreply.github.com>
2024-02-11 13:22:11 -0500

    Update EnemyAI.cs

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EnemyAI : MonoBehaviour
 {
+    private enum ObjectType { Property, Coworker, Chicken };
+    private GameManager gameManager;
+    [SerializeField] int pointValue = 100;
+    [SerializeField] Transform canvas;
+    [SerializeField] ObjectType objectType;
+    [SerializeField] GameObject pointTextPrefab;
+    private SpriteRenderer sprite;
+    private GameObject pointText;
+
     public Transform player;
     public Transform avoid;
 
@@ -11,12 +21,14 @@ public class EnemyAI : MonoBehaviour
     public int timer;
     private bool backTrack;
 
+    public int health;
 
     void Start()
     {
         backTrack = false;
-
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         player = GameObject.FindWithTag("Player").transform;
+        canvas = GameObject.Find("WorldCanvas").GetComponent<Transform>();
     }
 
     void FixedUpdate()
@@ -51,5 +63,45 @@ public class EnemyAI : MonoBehaviour
             backTrack = true;
         }
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Destroyer"))
+        {
+           
+            Health(10);
+        }
+    }
+
+    private void OnTriggerstay2D(Collider2D other)
+    {
+        if (other.CompareTag("Destroyer"))
+        {
+            sprite.color = Color.red;
+        }
+    }
+
+
+
+    void Health(int h)
+    {
+        health -= h;       
+
+        if (health <= 0)
+        {
+            gameManager.score += pointValue;
+
+            if (objectType == ObjectType.Property) { gameManager.propDmg += 1; }
+            else if (objectType == ObjectType.Coworker) { gameManager.hrViol += 1; }
+            else if (objectType == ObjectType.Chicken) { gameManager.chicken += 1; }
+            else { Debug.LogError(gameObject + " invalid object type: " + objectType); }
+
+            pointText = Instantiate(pointTextPrefab, canvas);
+            pointText.transform.position = transform.position;
+            pointText.GetComponent<TextMeshPro>().text = pointValue.ToString();
+
+            Destroy(this.gameObject);
+        }
     }
 }
